f078bddd7297661bc391aff243ead6c68ce18169
Johnson <olz264@14109fd57b4f.cof.ds.capitalone.com>
2013-09-13 10:13:41 -0400

    Initial Commit

---
+++ transferHack/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  transferHack
+//
+//  Created by Johnson, John (CONT) on 9/13/13.
+//  Copyright (c) 2013 Capital One. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
