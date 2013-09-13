b76f056faad6879467054508d08d199c4f649785
Johnson <olz264@14109fd57b4f.local>
2013-09-13 10:28:34 -0400

    Transfer View Controller Commit

---
+++ transferHack/TransferViewController.h
@@ -0,0 +1,13 @@
+//
+//  TransferViewController.h
+//  transferHack
+//
+//  Created by Johnson, John (CONT) on 9/13/13.
+//  Copyright (c) 2013 Capital One. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TransferViewController : UIViewController
+
+@end
---
+++ transferHack/TransferViewController.m
@@ -0,0 +1,38 @@
+//
+//  TransferViewController.m
+//  transferHack
+//
+//  Created by Johnson, John (CONT) on 9/13/13.
+//  Copyright (c) 2013 Capital One. All rights reserved.
+//
+
+#import "TransferViewController.h"
+
+@interface TransferViewController ()
+
+@end
+
+@implementation TransferViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
